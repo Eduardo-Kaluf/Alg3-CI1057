7002e6f5092cccdfcc027ca80c86463c0ebf10b2
Eduardo-Kaluf <eduardokaluf1@gmail.com>
2025-04-29 22:09:20 -0300

    Tests

--- RB/testes.sh
+++ RB/testes.sh
@@ -4,18 +4,38 @@ clear
 
 make
 
-./myrb < ./testes/teste1 > ./testes/teste1.out
-diff ./testes/teste1.sol ./testes/teste1.out
-./myrb < ./testes/teste2 > ./testes/teste2.out
-diff ./testes/teste2.sol ./testes/teste2.out
-./myrb < ./testes/teste3 > ./testes/teste3.out
-diff ./testes/teste3.sol ./testes/teste3.out
-./myrb < ./testes/teste4 > ./testes/teste4.out
-diff ./testes/teste4.sol ./testes/teste4.out
+./myrb < ./testes/moodle_tests/teste1 > ./testes/moodle_tests/teste1.out
+diff ./testes/moodle_tests/teste1.sol ./testes/moodle_tests/teste1.out
+./myrb < ./testes/moodle_tests/teste2 > ./testes/moodle_tests/teste2.out
+diff ./testes/moodle_tests/teste2.sol ./testes/moodle_tests/teste2.out
+./myrb < ./testes/moodle_tests/teste3 > ./testes/moodle_tests/teste3.out
+diff ./testes/moodle_tests/teste3.sol ./testes/moodle_tests/teste3.out
+./myrb < ./testes/moodle_tests/teste4 > ./testes/moodle_tests/teste4.out
+diff ./testes/moodle_tests/teste4.sol ./testes/moodle_tests/teste4.out
 
-rm ./testes/teste1.out
-rm ./testes/teste2.out
-rm ./testes/teste3.out
-rm ./testes/teste4.out
+rm ./testes/moodle_tests/teste1.out
+rm ./testes/moodle_tests/teste2.out
+rm ./testes/moodle_tests/teste3.out
+rm ./testes/moodle_tests/teste4.out
+
+./myrb < ./testes/personal_tests/teste1 > ./testes/personal_tests/teste1.out
+diff ./testes/personal_tests/teste1.sol ./testes/personal_tests/teste1.out
+./myrb < ./testes/personal_tests/teste2 > ./testes/personal_tests/teste2.out
+diff ./testes/personal_tests/teste2.sol ./testes/personal_tests/teste2.out
+./myrb < ./testes/personal_tests/teste3 > ./testes/personal_tests/teste3.out
+diff ./testes/personal_tests/teste3.sol ./testes/personal_tests/teste3.out
+./myrb < ./testes/personal_tests/teste4 > ./testes/personal_tests/teste4.out
+diff ./testes/personal_tests/teste4.sol ./testes/personal_tests/teste4.out
+./myrb < ./testes/personal_tests/teste5 > ./testes/personal_tests/teste5.out
+diff ./testes/personal_tests/teste5.sol ./testes/personal_tests/teste5.out
+./myrb < ./testes/personal_tests/teste6 > ./testes/personal_tests/teste6.out
+diff ./testes/personal_tests/teste6.sol ./testes/personal_tests/teste6.out
+
+rm ./testes/personal_tests/teste1.out
+rm ./testes/personal_tests/teste2.out
+rm ./testes/personal_tests/teste3.out
+rm ./testes/personal_tests/teste4.out
+rm ./testes/personal_tests/teste5.out
+rm ./testes/personal_tests/teste6.out
 
 make clean

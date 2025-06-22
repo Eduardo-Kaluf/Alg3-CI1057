0de35433f4e5e48966a5d3b418c1a75d1c215a17
Eduardo-Kaluf <eduardokaluf1@gmail.com>
2025-06-22 20:43:41 -0300

    -help

--- HASH/main.c
+++ HASH/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "hashtable.h"
 
 
-int main() {
+int main(int argc, char **argv) {
+
+    if (argc == 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+            printf("Nome: Eduardo Kaluf\nGRR: GRR20241770\nlogin: ek24\nNome: Sergio Sivonei de Sant'ana Filho\nGRR: GRR20242337\nlogin: sssaf24\n");
+        return 0;
+    }
+
     char instruction;
     int key;
-	
+
     struct hashtable *hashtable = create_hashtable(SIZE);
 	
     while (scanf("%c", &instruction) != EOF) {
